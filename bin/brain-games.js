70be8addaaafa124ed91d473132092fc063e02dd
Konovalchik office <ikonovalchik@redloft.ru>
2020-11-02 17:36:25 +0300

    Create brain even game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import async from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-console.log('May I have your name?');
 async();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import promptly from 'promptly';
 
 export default async () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('May I have your name?');
   const name = await promptly.prompt('Your answer: ');
   console.log(`Hello, ${name}!`);
 };
